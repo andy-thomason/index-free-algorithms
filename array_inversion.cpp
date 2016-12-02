261baea9067b69f4f05f010225ee4339276c072b
andy-thomason <andy@andythomason.com>
2016-12-02 11:00:20 +0000

    finally gained some improvement

--- array_inversion.cpp
+++ array_inversion.cpp
@@ -6,6 +6,28 @@
 #include <chrono>
 #include <future>
 #include <iostream>
+
+class generator {
+  long long seed_ = 0x123456789abcdef1;
+public:
+  size_t operator()() {
+    seed_ = ((seed_ >> 63) & 0xfedcba987654321f) ^ (seed_ << 1);
+    return (size_t)seed_;
+  };
+  typedef size_t result_type;
+  size_t min() { return 0; }
+  size_t max() { return (size_t)-1; }
+};
+
+size_t next_power_of_two(size_t v) {
+  v--;
+  v |= v >> 1;
+  v |= v >> 2;
+  v |= v >> 4;
+  v |= v >> 8;
+  v |= v >> 16;
+  return v + 1;
+}
  
 int main()
 {
@@ -13,11 +35,11 @@ int main()
   std::vector<size_t> v(vsize);
   std::iota(v.begin(), v.end(), 0);
 
-  std::random_device rd;
-  std::ranlux24_base g(rd());
+  generator g;
   std::shuffle(v.begin(), v.end(), g);
 
   std::vector<size_t> v2(vsize);
+
   {
     auto start = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i != vsize; ++i) {
@@ -30,21 +52,18 @@ int main()
   {
     auto start = std::chrono::high_resolution_clock::now();
 
-    {
-      std::array<std::thread, 8> asyncs;
-      for (size_t core = 0; core != asyncs.size(); ++core) {
-        asyncs[core] = std::thread([core, vsize, &v2, &v]{
-          size_t imin = core*(vsize/8);
-          size_t imax = std::min(vsize, imin+(vsize/8));
-          for (size_t i = imin; i != imax; ++i) {
-            v2[v[i]] = i;
-          }
-        });
-      }
-      for (size_t core = 0; core != asyncs.size(); ++core) {
-        asyncs[core].join();
-      }
-      
+    std::array<std::thread, 8> threads;
+    for (size_t core = 0; core != threads.size(); ++core) {
+      threads[core] = std::thread([core, vsize, &v2, &v]{
+        size_t imin = core*(vsize/8);
+        size_t imax = std::min(vsize, imin+(vsize/8));
+        for (size_t i = imin; i != imax; ++i) {
+          v2[v[i]] = i;
+        }
+      });
+    }
+    for (size_t core = 0; core != threads.size(); ++core) {
+      threads[core].join();
     }
     auto end = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration<double, std::nano>(end - start).count() / vsize << "\n";
@@ -52,9 +71,10 @@ int main()
 
   {
     auto start = std::chrono::high_resolution_clock::now();
-    size_t block_size = 0x10000;
+    size_t block_size = next_power_of_two(vsize / 0x200);
     std::vector<size_t> dest(vsize/block_size+1);
     std::vector<size_t> tmp(block_size);
+    printf("%d\n", (int)dest.size());
 
     for (size_t i = 0; i != dest.size(); ++i) {
       dest[i] = i * block_size;
@@ -65,6 +85,7 @@ int main()
       v2[dest[value/block_size]++] = i * block_size + value % block_size;
     }
 
+    auto mid = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < vsize; i += block_size) {
       size_t jmax = std::min(vsize - i, block_size);
       for (size_t j = 0; j != jmax; ++j) {
@@ -77,16 +98,7 @@ int main()
     }
     auto end = std::chrono::high_resolution_clock::now();
 
-    /*for (size_t i = 0; i != vsize; ++i) {
-      std::cout << v[i] << " ";
-    }
-    std::cout << "\n";
-
-    for (size_t i = 0; i != vsize; ++i) {
-      std::cout << v2[i] << " ";
-    }
-    std::cout << "\n";*/
-
+    std::cout << std::chrono::duration<double, std::nano>(mid - start).count() / vsize << "\n";
     std::cout << std::chrono::duration<double, std::nano>(end - start).count() / vsize << "\n";
   }
 }
